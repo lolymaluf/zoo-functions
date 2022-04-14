67d39d3127ded052fed2c7844159398fd2b88dec
Loly Maluf <lolymaluf@gmail.com>
2022-04-14 18:30:42 -0300

    Adiciona Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,24 @@
-const data = require('../data/zoo_data');
+/* 1. Implemente a função getSpeciesByIds
+Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+Observações técnicas
+O parâmetro desta função pode ser alterado para atender ao requisito proposto. */
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+/* MINHAS OBSERVAÇÕES: Utilização do rest pois principal uso é função com vários parametros */
+
+function getSpeciesByIds(...ids) {
+  /* let BuscaDeEspecies = species.filter((specie) => specie.id); */
+  if (ids === 0) {
+    return [];
+  }
+    if (ids !== 0) {
+      return ids.map((callback) => species.find((idNovo) => idNovo.id === callback));
+    }
+  }
+
+/* map invoca a callback, faz manipulação dos elementos e retorna array novo diferente do atual. Estou usando para percorrer e comparar */
+/* Segundo if, pega a callback e compara para ver se tem no objeto se tem o id */
 
 module.exports = getSpeciesByIds;
