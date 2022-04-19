d15c88ac4c0a3bb69a438a530ed11580cbb7195a
Loly Maluf <lolymaluf@gmail.com>
2022-04-18 21:40:10 -0300

    Adiciona comentarios requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,4 +1,4 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 /* 4. Implemente a função getRelatedEmployees
 Considerando a boa prática de dividir o código em partes menores, apresentamos a função getRelatedEmployees em que você deverá dividí-la em duas funções:
@@ -18,10 +18,11 @@ function isManager(id) {
 }
 /* Apenas procurei no array se o funcionario é ou nao gerente */
 /* Se voltar como true, executa a segunda parte da função abaixo (para o return) */
-
+/* FONTES: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map */
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === false) {
-    throw new Error("O id inserido não é de uma pessoa colaboradora gerente!");
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   return data.employees
     .filter((idDoGerente) => idDoGerente.managers.includes(managerId))
@@ -30,5 +31,5 @@ function getRelatedEmployees(managerId) {
         `${responsavelPor.firstName} ${responsavelPor.lastName}`,
     );
 }
-
+/* Se for gerente, pego pelo id do gerente, e vejo se possui o id. Depois mapear quais são os funcionários dele para ver quem ele é responsável */
 module.exports = { isManager, getRelatedEmployees };
