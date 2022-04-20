13dea71987c84447e1933b2943e0a0f558fffaf1
Loly Maluf <lolymaluf@gmail.com>
2022-04-20 18:10:59 -0300

    Adiciona enunciado requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,5 +1,12 @@
 const data = require('../data/zoo_data');
 
+/* 9. Implemente a função getOldestFromFirstSpecies
+A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
+
+O que será avaliado
+
+Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie. */
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
